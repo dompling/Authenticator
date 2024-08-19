092b807f8d527f1ac03b313da89cfccf8c51f5e3
dompling <domplingyaya@gmail.com>
2024-08-19 16:21:54 +0800

    update

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,26 +1,24 @@
 const path = require("path");
-const { merge } = require('webpack-merge');
-const common = require('./webpack.config.js');
+const { merge } = require("webpack-merge");
+const common = require("./webpack.config.js");
 const { ProvidePlugin } = require("webpack");
 
 module.exports = merge(common, {
-  entry: {
-    test: "./src/test.ts",
-  },
+  entry: {},
   // Polyfills for mocha and sinon
   resolve: {
     fallback: {
-      "util": require.resolve("util/"),
-      "buffer": require.resolve('buffer/'),
-      "stream": require.resolve("stream-browserify")
-    }
+      util: require.resolve("util/"),
+      buffer: require.resolve("buffer/"),
+      stream: require.resolve("stream-browserify"),
+    },
   },
   plugins: [
     new ProvidePlugin({
-      process: 'process/browser.js',
+      process: "process/browser.js",
     }),
     new ProvidePlugin({
-      Buffer: ['buffer', 'Buffer'],
+      Buffer: ["buffer", "Buffer"],
     }),
   ],
   module: {
